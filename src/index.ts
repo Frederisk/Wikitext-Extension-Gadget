a40222c76dda57421fe41a3753d495361e3b43d3
Rowe Wilson Frederisk Holme <frederisk@outlook.com>
2021-07-25 22:28:08 +0800

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,37 @@
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
-function hello(world: string = "world"): string {
-  console.log(world)
-  return `Hello ${world}! ` + $("<br/>").toString();
-}
-hello();
+jQuery((): void => {
+  const pageName: String = window.mw.config.get('wgPageName');
+  const skinName: String = window.mw.config.get('skin');
+  if (!pageName || !skinName) {
+    return undefined;
+  }
+
+  const editButton: JQuery<HTMLElement> = $('#ca-edit');
+  if (editButton.length <= 0) {
+    return undefined;
+  }
+
+  const link = $('<a>').attr('href', 'code://').text('Open in VSCode');
+  const button = $('<li>').attr('id', 'vscode-wikitext-button');
+
+  if (skinName !== 'minerva') {
+    button.append(link);
+
+    editButton.parent().after(button);
+  }
+  else {
+    const span = $('<span>');
+    span.addClass('page-actions-menu__list-item');
+    link.addClass('mw-ui-icon mw-ui-icon-element mw-ui-icon-wikimedia-edit-base20 mw-ui-icon-with-label-desktop');
+
+    button.css({ 'align-items': 'center', 'display': 'flex' });
+    link.css('vertical-align', 'middle');
+
+    span.append(link);
+    button.append(span);
+
+    editButton.parent().after(button);
+  }
+});
