5058489d7087c3a354319221c9d134bce12564f1
Rowe Wilson Frederisk Holme <frederisk@outlook.com>
2021-08-30 05:03:46 +0800

    Add Editability check

--- src/index.ts
+++ src/index.ts
@@ -5,35 +5,33 @@
 
 /* eslint-disable @typescript-eslint/naming-convention */
 jQuery.when(window.mw.loader.using('mediawiki.util'), $.ready).then((): void => {
-  function getCodeLink(): string {
-    // get scheme, `vscode.env.uriScheme`
-    const scheme = 'vscode-insiders'; // 'vscode-insiders', 'vscode', undefined
-    // for codespace or code-server, you need invoke `await vscode.env.asExternalUri`
-    const codespaceScheme = 'https://*.github.dev';
-    const head: string = scheme ? `${scheme}://` : `${codespaceScheme}/`;
-
-    const extensionID = 'rowewilsonfrederiskholme.wikitext';
-    const actionPath = '/PullPage';
-
-    const args: Record<string, string> = {
-      RemoteBot: 'true',
-      TransferProtocol: window.location.protocol,
-      SiteHost: window.mw.config.get('wgServer'),
-      APIPath: window.mw.util.wikiScript('api'),
-      Title: window.mw.config.get('wgPageName')
-    };
-
-    return `${head}${extensionID}${actionPath}?${new URLSearchParams(args).toString()}`;
+  if (!mw.config.get("wgIsProbablyEditable") || !($('#ca-viewsource').length > 0)) {
+    return undefined;
   }
 
   const text = 'Open in VSCode';
   const gadgetID = 'wikitext-extension-gadget';
   const tooltip = 'Open this page in VSCode';
+  // get scheme, `vscode.env.uriScheme`
+  const scheme = 'vscode-insiders'; // 'vscode-insiders', 'vscode', undefined
+  // for codespace or code-server, you need invoke `await vscode.env.asExternalUri`
+  // const codespaceScheme = 'https://*.github.dev';
+  // const head: string = scheme ? `${scheme}://` : `${codespaceScheme}/`;
+  const extensionID = 'rowewilsonfrederiskholme.wikitext';
+  const actionPath = '/PullPage';
+  const args: Record<string, string> = {
+    RemoteBot: 'true',
+    TransferProtocol: window.location.protocol,
+    SiteHost: window.mw.config.get('wgServer'),
+    APIPath: window.mw.util.wikiScript('api'),
+    Title: window.mw.config.get('wgPageName')
+  };
+
   const isMinerva: boolean = window.mw.config.get('skin') === 'minerva';
 
   window.mw.util.addPortletLink(
     isMinerva ? 'p-tb' : 'p-views',
-    getCodeLink(),
+    `${scheme}${extensionID}${actionPath}?${new URLSearchParams(args).toString()}`,
     text,
     gadgetID,
     tooltip,
