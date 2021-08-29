53468663c186acc59e39447a0e6c153098f6dca1
Rowe Wilson Frederisk Holme <frederisk@outlook.com>
2021-08-29 21:32:58 +0800

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -7,9 +7,9 @@
 jQuery.when(window.mw.loader.using('mediawiki.util'), $.ready).then((): void => {
   function getCodeLink(): string {
     // get scheme, `vscode.env.uriScheme`
-    const scheme: string | undefined = 'vscode-insiders'; // 'vscode-insiders', 'vscode', undefined
+    const scheme = 'vscode-insiders'; // 'vscode-insiders', 'vscode', undefined
     // for codespace or code-server, you need invoke `await vscode.env.asExternalUri`
-    const codespaceScheme: string | undefined = 'https://*.github.dev';
+    const codespaceScheme = 'https://*.github.dev';
     const head: string = scheme ? `${scheme}://` : `${codespaceScheme}/`;
 
     const extensionID = 'rowewilsonfrederiskholme.wikitext';
@@ -26,18 +26,17 @@ jQuery.when(window.mw.loader.using('mediawiki.util'), $.ready).then((): void => 
     return `${head}${extensionID}${actionPath}?${new URLSearchParams(args).toString()}`;
   }
 
-  const text = "Open in VSCode";
-  const gadgetID = "wikitext-extension-gadget";
-  const tooltip = "Open this page in VSCode";
-
-  const skinName: string = window.mw.config.get('skin');
+  const text = 'Open in VSCode';
+  const gadgetID = 'wikitext-extension-gadget';
+  const tooltip = 'Open this page in VSCode';
+  const isMinerva: boolean = window.mw.config.get('skin') === 'minerva';
 
   window.mw.util.addPortletLink(
-    skinName === "minerva" ? 'p-tb' : 'p-views',
+    isMinerva ? 'p-tb' : 'p-views',
     getCodeLink(),
     text,
     gadgetID,
     tooltip,
     undefined,
-    '#ca-history');
+    isMinerva ? undefined : '#ca-history');
 });
