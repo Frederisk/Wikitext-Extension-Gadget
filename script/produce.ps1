0df61efe4dbfb848ec421f379bbf0433baebc2ef
M Rifan <20435546+fenymufyd@users.noreply.github.com>
2021-09-21 21:34:18 +0800

    add vscodium

--- script/produce.ps1
+++ script/produce.ps1
@@ -4,11 +4,14 @@ yarn run compile;
 
 $jsInsider = Get-Content -Path ".\dist\index.js";
 $jsCode = $jsInsider.Replace("vscode-insiders", "vscode");
+$jsCodium = $jsInsider.Replace("vscode-insiders", "vscodium");
 
 $pathInsider = ".\dist\index-vscode-insiders.js";
 $pathCode = ".\dist\index-vscode.js";
+$pathCodium = ".\dist\index-vscodium.js";
 
-New-Item -Path $pathInsider, $PathCode -ItemType File -Force;
+New-Item -Path $pathInsider, $PathCode, $pathCodium -ItemType File -Force;
 
 Set-Content -Path $pathInsider -Value $jsInsider
 Set-Content -Path $pathCode -Value $jsCode
+Set-Content -Path $pathCodium -Value $jsCodium
